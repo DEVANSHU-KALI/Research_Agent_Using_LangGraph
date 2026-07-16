4fcda363bf9598797a42257ef2228befd86528ad
DEVANSHU-KALI <devanshu9666@gmail.com>
2026-07-17 02:01:15 +0530

    removed the one time folder

--- one_time/embedding_model.py
+++
@@ -1,5 +0,0 @@
-from langchain_huggingface import HuggingFaceEmbeddings
-
-embedding_model = HuggingFaceEmbeddings(
-    model= "sentence-transformers/all-mpnet-base-v2"
-)
--- one_time/text_chunker.py
+++
@@ -1,8 +0,0 @@
-from langchain_experimental.text_splitter import SemanticChunker
-from one_time.embedding_model import embedding_model
-
-text_splitter =  SemanticChunker(
-    embedding_model,
-    breakpoint_threshold_type= 'percentile',
-    breakpoint_threshold_amount= 75
-)
